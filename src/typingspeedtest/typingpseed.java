0952c1ca6c3f7daa198edfba8d7a76f2b9770993
Arran Keogh <keogh46.2@gmail.com>
2025-09-29 14:46:17 +0100

    added results class

--- src/typingspeedtest/typingpseed.java
+++ src/typingspeedtest/typingpseed.java
@@ -27,14 +27,15 @@ public class typingpseed {
 	
 	public static void startTest() {
 		String[] phrases = {
-				"please give me a job",
-				"i really want money",
-				"I promise to work as hard as I can",
-				"I will never ever let you down",
-				"Thank you for your time"
+				"please give me a job i will be so happy",
+				"i really think i would fit in with your team",
+				"if i am bad i will pay you ten grand",
+				"i dont mean that i just need ten word sentences",
+				"but i do mean the job part please please please"
 			};
 		String[] input = new String[5];
 		long[] timeTaken = new long[5];
+		Result results[] = new Result[5];
 		
 		for(int i=0;i<5;i++) {
 		System.out.println("Phrase " + (i+1) + ": " + phrases[i]);
@@ -43,17 +44,65 @@ public class typingpseed {
 		input[i] = scan.nextLine();
 		long endTime = System.currentTimeMillis();
 		timeTaken[i] = endTime - startTime;
+		results[i] = new Result((double)timeTaken[i]/1000, calculateWPM(timeTaken[i]), calculateAccuracy(phrases[i], input[i]));
 		clearConsole();
 		}
 		
-		for(int i=0;i<5;i++) {
-			System.out.println("Phrase " + (i+1) + ": " + phrases[i]);
-			System.out.println("You typed: " + input[i]);
-			System.out.println("Time taken: " + timeTaken[i]/1000.0 + " seconds");
-			System.out.println("----------------------******----------------------");
-		}
-	
-
+		System.out.println("TEST COMPLETE!");
+		
 	
 	}
+	
+		public static int calculateWPM(long time) {
+			int wpm = (int) (time/10000)*60;
+			return wpm;
+		}
+		
+		public static int calculateAccuracy(String phrase, String input) {
+			char[] phraseChars = phrase.toCharArray();
+			char[] inputChars = input.toCharArray();
+			int correctChars = 0;
+			
+			for(int i=0;i<phraseChars.length && i<inputChars.length;i++) {
+				if(phraseChars[i] == inputChars[i]) {
+					correctChars++;
+				}
+			}
+			int accuracy = (int) ((double)correctChars/phraseChars.length*100);
+			return accuracy;
+		}
+		
+		static class Result{
+			
+			double time=0;
+			int wpm=0;
+			int accuracy=0;
+			
+			public Result(double time, int wpm, int accuracy) {
+				this.time = time;
+				this.wpm = wpm;
+				this.accuracy = accuracy;			
+			}
+			
+			public double getTime() {
+				return this.time;
+			}
+			public int getWpm() {
+				return this.wpm;
+			}
+			public int getAccuracy() {
+				return this.accuracy;
+			}
+			public void setTime(double time) {
+				this.time = time;
+			}
+			public void setWpm(int wpm) {
+				this.wpm = wpm;
+			}
+			public void setAccuracy(int accuracy) {
+				this.accuracy = accuracy;
+			}
+			
+			
+		}
 }
